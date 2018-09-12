f31682d7cc38b0d3343b2394c7242f6b37d0c21f
Ethan <ls4int@gmail.com>
2018-09-12 18:45:45 +0800

    2 way invoke consul service

--- app/src/main/java/com/ethan/stage/controller/ConsulController.java
+++ app/src/main/java/com/ethan/stage/controller/ConsulController.java
@@ -1,13 +1,39 @@
 package com.ethan.stage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsulController {
 
+    @Autowired private DiscoveryClient discoveryClient;
+    @Autowired private LoadBalancerClient loadBalancer;
+
     @RequestMapping("/hello")
     public String hello() {
         return "reply from app";
+    }
+
+    @RequestMapping("/consul/services/")
+    public Object getConsulServices() {
+        return discoveryClient.getInstances("consul-service");
+    }
+
+    @RequestMapping("/consul/invoke/")
+    public String invokeConsul() {
+        // 显式使用客户端负载均衡, 隐式见 app2 的 ConsulController
+        ServiceInstance serviceInstance = loadBalancer.choose("consul-service");
+        System.out.println("consul-service地址：" + serviceInstance.getUri());
+
+        String callServiceResult =
+                new RestTemplate()
+                        .getForObject(serviceInstance.getUri().toString() + "/hello", String.class);
+        System.out.println("consul-service调用结果: " + callServiceResult);
+        return callServiceResult;
     }
 }
--- app2/src/main/java/com/ethan/stage/app2/ConsulController.java
+++ app2/src/main/java/com/ethan/stage/app2/ConsulController.java
@@ -1,13 +1,26 @@
 package com.ethan.stage.app2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsulController {
 
+    // 这个RestTemplate由注解了LoadBalanced的bean创建
+    @Autowired RestTemplate loadBalanceRestTemplate;
+
     @RequestMapping("/hello")
     public String hello() {
         return "reply from app---2";
+    }
+
+    @RequestMapping("/consul/invoke/")
+    public String invokeConsul() {
+        // 隐式调用客户端负载均衡
+        String result =
+                loadBalanceRestTemplate.getForObject("http://consul-service/hello", String.class);
+        return result;
     }
 }
