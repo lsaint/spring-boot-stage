b763c7cee986c6f86a3548674789496349f30bc5
Ethan <ls4int@gmail.com>
2018-09-14 13:52:31 +0800

    zuul

---
+++ zuul/src/main/java/com/ethan/stage/zuul/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.ethan.stage.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/*
+ * Zuul的路由规则如下：
+ * http://ZUUL_HOST:ZUUL_PORT/consul上的serviceName/**
+ * 会被转发到serviceName对应的微服务
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
